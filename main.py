65399e1ad10c8873d1df99e19d2f650f49d4d6c0
NelsonHMalbone <nhmalbone@icloud.com>
2024-04-15 12:14:45 -0400

    added the question to complete started to add the code for inputing a message and looping over the bitmap

--- main.py
+++ main.py
@@ -27,3 +27,12 @@ bitmap = """
                     ***                                       *    *
                     **     *                    *
 ...................................................................."""
+
+print('Bitmap Massage')
+print("Enter the message to display with the bitmap")
+
+message = input("> ")
+if message == "":
+    sys.exit()
+
+# loop iver each line in the bitmap
