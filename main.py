3836328e8ccf27b6943856234fc67d88d0565380
NelsonHMalbone <nhmalbone@icloud.com>
2024-04-16 09:08:23 -0400

    project works and completed

--- main.py
+++ main.py
@@ -36,3 +36,13 @@ if message == "":
     sys.exit()
 
 # loop iver each line in the bitmap
+for line in bitmap.splitlines():
+    # loop over each char in the line:
+    for i, bit in enumerate(line):
+        if bit == ' ':
+            # print an empty space since theres a space in the bitmap
+            print(' ', end='')
+        else:
+            # print a char form the mess
+            print(message[i % len(message)], end='')
+    print() # prints a newline
