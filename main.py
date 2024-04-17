a23a3321f9e3feee3357ffe409cc6d10a5422215
Nelson Malbone <nhmalbone@icloud.com>
2024-04-17 19:23:37 -0400

    fixed grammer issues

--- main.py
+++ main.py
@@ -35,14 +35,14 @@ message = input("> ")
 if message == "":
     sys.exit()
 
-# loop iver each line in the bitmap
+# loop over each line in the bitmap
 for line in bitmap.splitlines():
     # loop over each char in the line:
     for i, bit in enumerate(line):
         if bit == ' ':
-            # print an empty space since theres a space in the bitmap
+            # print an empty space since there's a space in the bitmap
             print(' ', end='')
         else:
             # print a char form the mess
             print(message[i % len(message)], end='')
-    print() # prints a newline
+    print()  # prints a newline
